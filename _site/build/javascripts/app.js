01ef9fc82d9df9323bb185b23d8bea3351cd4f37
Aaron White <bigabsu@gmail.com>
2013-06-24 09:12:18 -0400

    fix for build on push

---
+++ _site/build/javascripts/app.js
@@ -0,0 +1,90 @@
+(function($){  
+  $(function(){
+    $(document).foundationMediaQueryViewer();
+    
+    $(document).foundationAlerts();
+    $(document).foundationAccordion();
+    $(document).tooltips();
+    $('input, textarea').placeholder();
+    
+    
+    
+    $(document).foundationButtons();
+    
+    
+    
+    $(document).foundationNavigation();
+    
+    
+    
+    $(document).foundationCustomForms();
+    
+    
+    
+      
+      $(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
+      
+    
+    
+    
+    $("#featured").orbit();
+    
+    
+    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
+    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
+    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
+    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+  });
+
+  
+  // side bar
+  $(window).on('scroll', function() {
+    $(".well.subnav").css({
+       position: $(this).scrollTop()<200?'relative':'fixed',
+       top: $(this).scrollTop()<200?'0px':'22px'
+    });
+
+});
+
+  var showChar = 100;
+  var ellipsestext = "...";
+  var moretext = "more";
+  var lesstext = "less";
+  $('.more').each(function() {
+      var content = $(this).html();
+
+      if(content.length > showChar) {
+
+          var c = content.substr(0, showChar);
+          var h = content.substr(showChar-1, content.length - showChar);
+
+          var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+          $(this).html(html);
+      }
+
+  });
+
+  $(".morelink").click(function(){
+      if($(this).hasClass("less")) {
+          $(this).removeClass("less");
+          $(this).html(moretext);
+      } else {
+          $(this).addClass("less");
+          $(this).html(lesstext);
+      }
+      $(this).parent().prev().toggle();
+      $(this).prev().toggle();
+      return false;
+  });
+
+function toggle_visibility(id) {
+ var e = document.getElementById(id);
+ if(e.style.display == 'block')
+    e.style.display = 'none';
+ else
+    e.style.display = 'block';
+}
+  
+})(jQuery);
